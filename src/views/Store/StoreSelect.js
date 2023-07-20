6f22ab54f63f64cc885b02e820ce2b9f5805a53e
haihung01 <104615629+haihung01@users.noreply.github.com>
2023-07-21 01:04:09 +0700

    button store&StoreAuction

--- src/views/Store/StoreSelect.js
+++ src/views/Store/StoreSelect.js
@@ -1,28 +1,59 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { View } from 'react-native-animatable'
-import { Text, TouchableOpacity } from 'react-native'
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import { Ionicons } from '@expo/vector-icons';
 
-function StoreSelect(props) {
+function StoreSelect() {
     const navigation = useNavigation();
 
     return (
-        <View>
-            <TouchableOpacity  style={{ width: '33%', margin: 0.8 }} onPress={() => {
-                navigation.navigate('StoreAuction');
-            }}>
-                <Text>Store Auction</Text>
+        <View style={styles.container}>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    navigation.navigate('Store');
+                }}
+            >
+                <Ionicons name="add-circle-outline" size={24} color="#fff" />
+                <Text style={styles.buttonText}>Store</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={{ width: '33%', margin: 0.8 }} onPress={() => {
-                navigation.navigate('Store');
-            }}>
-                <Text>Store </Text>
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                    navigation.navigate('StoreAuction');
+                }}
+            >
+                <Ionicons name="chatbubbles-outline" size={24} color="#fff" />
+                <Text style={styles.buttonText}>Store Auction</Text>
             </TouchableOpacity>
         </View>
-    )
+    );
 }
 
-StoreSelect.propTypes = {}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+    },
+    button: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '80%',
+        marginVertical: 10,
+        paddingVertical: 12,
+        borderRadius: 8,
+        backgroundColor: '#0099ff',
+    },
+    buttonText: {
+        marginLeft: 8,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+    },
+});
 
-export default StoreSelect
+export default StoreSelect;
